c007f7b81beba5bf74270169ee2143ed964c7de3
eug-vs <eugene@eug-vs.xyz>
2025-06-10 19:48:00 +0200

    fix: resolve race conditions in stockfish analysis

--- src/workers/stockfish/index.ts
+++ src/workers/stockfish/index.ts
@@ -1,6 +1,5 @@
 import { store } from "@/app/store";
-import { BrowserWorker } from "@effect/platform-browser";
-import * as EffectWorker from "@effect/platform/Worker";
+import { Chess } from "chess.js";
 import {
   Context,
   Effect,
@@ -9,6 +8,7 @@ import {
   LogLevel,
   ManagedRuntime,
   pipe,
+  Pool,
   Stream,
 } from "effect";
 
@@ -24,59 +24,108 @@ export interface EngineEvaluation {
 
 class StockfishPool extends Context.Tag("StockfishPool")<
   StockfishPool,
-  EffectWorker.WorkerPool<EngineRequest, EngineEvaluation>
+  Pool.Pool<Worker>
 >() {}
 
-const stockfishLayer = Layer.scoped(
-  StockfishPool,
-  pipe(
-    EffectWorker.makePool<EngineRequest, EngineEvaluation, never>({
-      minSize: 0,
-      maxSize: navigator.hardwareConcurrency - 1,
-      timeToLive: "1 minutes",
-    }),
-    Effect.provide(
-      BrowserWorker.layer(
-        () => new Worker(new URL("./stockfish.ts", import.meta.url)),
+const pool = Pool.makeWithTTL({
+  min: 0,
+  max: navigator.hardwareConcurrency - 1,
+  timeToLive: "30 seconds",
+  concurrency: 1,
+  acquire: Effect.acquireRelease(
+    Effect.sync(
+      () => new Worker(new URL("/stockfish.wasm.js", location.origin)),
+    ).pipe(Effect.tap((worker) => Effect.log("Spawned new worker", worker))),
+    (worker) =>
+      Effect.sync(() => worker.terminate()).pipe(
+        Effect.tap((worker) => Effect.log("Terminating worker", worker)),
       ),
-    ),
-    Logger.withMinimumLogLevel(LogLevel.Debug),
-    Effect.provide(Logger.pretty),
   ),
-);
+});
+
+const stockfishLayer = Layer.scoped(StockfishPool, pool);
 
 const StockfishRuntime = ManagedRuntime.make(stockfishLayer);
 
+function checkMoveValidity(fen: string, lan: string) {
+  return Effect.try(() => {
+    const board = new Chess(fen);
+    board.move(lan);
+  }).pipe(
+    Effect.catchAll((e) =>
+      Effect.logError(
+        `Could not apply best move ${lan} for fen ${fen}, ${e.message}`,
+      ),
+    ),
+  );
+}
+
 export async function analyzePositions(fens: string[], depth = 20) {
-  return StockfishRuntime.runPromise(
-    pipe(
-      StockfishPool,
-      Effect.flatMap((pool) =>
-        Effect.forEach(
-          fens,
-          (fen) =>
-            pool
-              .execute({
-                fen,
-                depth,
-              })
-              .pipe(
-                Stream.runForEach((engineEval) =>
-                  Effect.sync(() =>
-                    store.send({
-                      type: "addEngineEval",
-                      fen,
-                      eval: engineEval,
-                    }),
-                  ),
-                ),
+  const analyzePosition = (fen: string, depth: number) =>
+    StockfishPool.pipe(
+      Effect.flatMap((pool) => pool.get),
+      Effect.ensuring(Effect.logDebug("analyzePosition finalizer")),
+      Effect.flatMap((stockfish) =>
+        pipe(
+          Effect.sync(() => {
+            stockfish.postMessage(`ucinewgame`);
+            stockfish.postMessage(`position fen ${fen}`);
+            stockfish.postMessage(`go depth ${depth}`);
+          }),
+          Effect.flatMap(() => {
+            let lastSeenDepth = 0;
+            return Stream.fromEventListener(stockfish, "message").pipe(
+              Stream.filter((event) => event instanceof MessageEvent),
+              Stream.map((event) => event.data),
+              Stream.filter((data) => typeof data === "string"),
+              Stream.tap(Effect.logDebug),
+              Stream.tapError(Effect.logError),
+              // Make sure messages are coming in the correct order
+              // This is actually preventing some very nasty bug
+              // that I don't even fully comprehend
+              Stream.dropUntil((uciResponse) =>
+                uciResponse.startsWith(`info depth ${lastSeenDepth + 1}`),
               ),
-          {
-            concurrency: "unbounded",
-          },
+              Stream.tap(() => Effect.sync(() => lastSeenDepth++)),
+              Stream.map((uciResponse) => {
+                const parts = uciResponse.split(" ");
+                const bestmove = parts[parts.indexOf("pv") + 1];
+                const score = Number(parts[parts.indexOf("cp") + 1]);
+                const depth = Number(parts[parts.indexOf("depth") + 1]);
+                return {
+                  score,
+                  depth,
+                  bestmove,
+                };
+              }),
+              Stream.tap((data) => checkMoveValidity(fen, data.bestmove)),
+              Stream.takeUntil((result) => result.depth === depth),
+              Stream.runForEach((engineResult) =>
+                Effect.sync(() => {
+                  store.send({
+                    type: "addEngineEval",
+                    fen,
+                    eval: engineResult,
+                  });
+                }),
+              ),
+            );
+          }),
+          Effect.withLogSpan("stockfish"),
         ),
       ),
+      Effect.scoped,
+      Effect.withLogSpan(`analyzePosition`),
+    );
+
+  return StockfishRuntime.runPromise(
+    pipe(
+      Effect.forEach(fens, (fen) => analyzePosition(fen, depth), {
+        concurrency: "unbounded",
+      }),
+      Effect.provide(Logger.pretty),
       Effect.tapError(Effect.logError),
+      Logger.withMinimumLogLevel(LogLevel.Debug),
     ),
   );
 }
