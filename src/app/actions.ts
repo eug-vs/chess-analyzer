bd1fdcc8fbf7accdfc6892a3bfc9d7d6a59883a1
eug-vs <eugene@eug-vs.xyz>
2025-06-10 03:37:59 +0200

    feat: proxy lichess requests via server

---
+++ src/app/actions.ts
@@ -0,0 +1,30 @@
+"use server";
+
+export async function fetchLichessPgns(
+  playerName = "eug_vs",
+  since = new Date("2025-06-01"),
+  perfType = ["blitz", "bullet"],
+) {
+  const TRUE = "true";
+  const FALSE = "false";
+  const searchParams = new URLSearchParams({
+    rated: TRUE,
+    tags: TRUE,
+    clocks: TRUE,
+    evals: TRUE,
+    opening: FALSE,
+    literate: FALSE,
+    since: Number(since).toString(),
+    perfType: encodeURI(perfType.join(",")),
+  });
+  const res = await fetch(
+    `https://lichess.org/games/export/${playerName}?${searchParams.toString()}`,
+    {
+      next: {
+        revalidate: 3600 * 24,
+      },
+    },
+  );
+  const text = await res.text();
+  return text.trim().split("\n\n\n");
+}
--- src/app/pgnParser.tsx
+++ src/app/pgnParser.tsx
@@ -1,35 +1,7 @@
 "use client";
 
 import { parsePgns } from "@/orchestrator";
-
-async function fetchLichessPgns(
-  playerName = "eug_vs",
-  since = new Date("2025-06-01"),
-  perfType = ["blitz", "bullet"],
-) {
-  const TRUE = "true";
-  const FALSE = "false";
-  const searchParams = new URLSearchParams({
-    rated: TRUE,
-    tags: TRUE,
-    clocks: TRUE,
-    evals: TRUE,
-    opening: FALSE,
-    literate: FALSE,
-    since: Number(since).toString(),
-    perfType: encodeURI(perfType.join(",")),
-  });
-  const res = await fetch(
-    `https://lichess.org/games/export/${playerName}?${searchParams.toString()}`,
-    {
-      next: {
-        revalidate: 0,
-      },
-    },
-  );
-  const text = await res.text();
-  return text.trim().split("\n\n\n");
-}
+import { fetchLichessPgns } from "./actions";
 
 export default function PGNParser() {
   return (
